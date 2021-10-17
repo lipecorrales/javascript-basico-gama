a15840dc5040398636ef43b41a1335ab0ea41f1b
Philippe Corrales <philippecorrales@gmail.com>
2021-10-17 18:53:53 -0300

    atualizaçoes

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,16 @@
-function soma() {
-    
+function soma(operadorA, operadorB) {
+    var resultadoC = operadorA + operadorB;
+    return resultadoC;
 }
+
+function olaGama(nome){
+    console.log('Ola Gama! Você é o ' + nome);
+}
+
+var resultadoDaSoma = soma(1, 2);
+var resultadodoNovoDaSoma   = soma(10, 66);
+
+console.log(resultadoDaSoma);
+console.log(resultadodoNovoDaSoma);
+
+var resultado = olaGama('Philippe');
